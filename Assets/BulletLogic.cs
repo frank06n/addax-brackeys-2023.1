56744563e5ebed621be49c84941771c5c117e38a
Pritam Das <dev.code38@gmail.com>
2023-02-14 13:58:54 +0530

    Movement & Fire Prototype 0
    WASD movement
    Forward & Backward Fire

---
+++ Assets/BulletLogic.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletLogic : MonoBehaviour
+{
+    private float damage;
+    private string shooterTag;
+
+    private bool unfired;
+    private Vector2 endPoint;
+    private Action onUnfireComplete;
+
+    public void Initialise(float damage, Vector2 speed, string shooterTag)
+    {
+        Initialise(damage, speed, shooterTag, false, Vector2.zero, null);
+    }
+    public void Initialise(float damage, Vector2 speed, string shooterTag, bool unfired, Vector2 endPoint, Action onUnfireComplete)
+    {
+        this.damage = damage;
+        GetComponent<Rigidbody2D>().velocity = speed;
+        this.shooterTag = shooterTag;
+        this.unfired = unfired;
+        this.endPoint = endPoint;
+        this.onUnfireComplete = onUnfireComplete;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!unfired) return;
+        if (Vector2.Distance(transform.position, endPoint) <= 0.2)
+        {
+            Destroy(gameObject);
+            if (onUnfireComplete != null) onUnfireComplete();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (unfired || collision.collider.CompareTag(shooterTag)) return;
+
+        Debug.Log("Bullet Hit: " + collision.collider.name);
+        Destroy(gameObject);
+
+        /*if (collision.collider.CompareTag("Player"))
+        {
+            LevelManager.instance.player.Damage(damage);
+        }
+        else if (collision.collider.CompareTag("EnemyTurret"))
+        {
+            collision.collider.GetComponent<EnemyTurretLogic>().Damage(damage);
+        }
+        else
+        {
+            // on hitting wall
+            SceneManager2.instance.sfxPlayer.Play("bullet_hit_platform");
+        }*/
+    }
+}
---
+++ Assets/GunLogic.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunLogic : MonoBehaviour
+{
+    public float BulletDamage;
+    public float BulletSpeed;
+    public GameObject BulletPrefab;
+
+    //[SerializeField] protected string BulletFireSfx;
+
+    private Transform BulletPoint;
+    public bool unfiring;
+
+    private void Awake()
+    {
+        BulletPoint = transform.GetChild(1);
+    }
+
+    protected virtual void OnAwake() { }
+
+    public void FireBullet()
+    {
+        Vector2 speed = (BulletPoint.position - transform.position).normalized * BulletSpeed;
+        GameObject bulletObj = Instantiate(BulletPrefab, BulletPoint.position, transform.rotation, transform);
+        bulletObj.transform.parent = null;// LevelManager.instance.BulletsHolder;
+
+        BulletLogic bullet = bulletObj.GetComponent<BulletLogic>();
+        bullet.Initialise(BulletDamage, speed, "Player");
+
+        Debug.Log(BulletPoint.position);
+
+        //SceneManager2.instance.sfxPlayer.Play(BulletFireSfx);
+    }
+    public void UnFireBullet()
+    {
+        Vector2 direction = (BulletPoint.position - transform.position).normalized;
+        RaycastHit2D pt = Physics2D.Raycast(BulletPoint.position, direction, 50, Physics2D.AllLayers, -10, 0);
+        
+        Vector2 speed = -direction * BulletSpeed;
+        GameObject bulletObj = Instantiate(BulletPrefab, pt.point, transform.rotation, transform);
+        bulletObj.transform.parent = null;// LevelManager.instance.BulletsHolder;
+
+        BulletLogic bullet = bulletObj.GetComponent<BulletLogic>();
+        bullet.Initialise(BulletDamage, speed, "Player", true, BulletPoint.position, () =>
+        {
+            this.unfiring = false;
+        });
+        this.unfiring = true;
+
+        //SceneManager2.instance.sfxPlayer.Play(BulletFireSfx);
+    }
+
+    public void LookTowards(Vector2 position, bool inverted = false)
+    {
+        float lookAngle = Vector2.SignedAngle(Vector2.right, position - (Vector2)transform.position);
+        if (inverted) lookAngle += 180;
+
+        LookTowards(lookAngle);
+    }
+
+    protected void LookTowards(float lookAngle)
+    {
+        if (lookAngle > 180) lookAngle -= 360;
+        if (lookAngle < -180) lookAngle += 360;
+        transform.rotation = Quaternion.Euler(0, 0, lookAngle);
+
+        float scaleY = (Mathf.Abs(lookAngle) <= 90) ? +1 : -1;
+        transform.localScale = new Vector3(1, scaleY, 1);
+    }
+}
