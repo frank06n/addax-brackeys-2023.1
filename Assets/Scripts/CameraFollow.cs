f46c5d949d8476ed507c3cf561c0d81af8bc0227
Pritam Das <dev.code38@gmail.com>
2023-02-16 00:32:34 +0530

    Camera Follow

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform player;
+    public float CameraFollowSmoothTime;
+    private Vector3 cameraVelocity;
+    void FixedUpdate()
+    {
+        Vector3 playerPos = player.position;
+        playerPos.z = transform.position.z;
+        //transform.position = playerPos;
+
+        Vector3.SmoothDamp(transform.position, playerPos, ref cameraVelocity, CameraFollowSmoothTime);
+        transform.position += cameraVelocity * Time.deltaTime;
+    }
+}
