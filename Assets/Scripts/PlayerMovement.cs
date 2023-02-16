a9e97b6a9ba217c481c72c766b8df4792a0d8c13
gay-cat <rajarshiroy2005456@gmail.com>
2023-02-16 23:21:44 +0530

    added player anim

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -17,7 +17,11 @@ public class PlayerMovement : MonoBehaviour
     private PickupLogic heldObject;
     private WeaponLogic weapon;
 
+    // Animator
+    private Animator animator;
 
+    // Boolean for checking if player moves
+    private bool isMoving;
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -26,10 +30,14 @@ public class PlayerMovement : MonoBehaviour
         holdPoint = transform.GetChild(0);
 
         pickupsAround = new List<PickupLogic>();
+
+        animator=GetComponent<Animator>();
+        isMoving = false;
     }
 
     private void Update()
     {
+        isMoving = false;
         if (weapon!=null && weapon.RequiresPause())
         {
             rb.velocity = Vector2.zero;
@@ -37,11 +45,26 @@ public class PlayerMovement : MonoBehaviour
         }
         
         Vector2 direction = Vector2.zero;
-        if (Input.GetKey(KeyCode.A)) direction.x = -1;
-        else if (Input.GetKey(KeyCode.D)) direction.x = +1;
-        if (Input.GetKey(KeyCode.W)) direction.y = +1;
-        else if (Input.GetKey(KeyCode.S)) direction.y = -1;
+        if (Input.GetKey(KeyCode.A))  {
+            direction.x = -1;
+            isMoving = true;
+        }
+        else if (Input.GetKey(KeyCode.D)) {
+            direction.x = +1;
+            isMoving = true;
+        }
+        if (Input.GetKey(KeyCode.W)) {
+            direction.y = +1;
+            isMoving = true;
+        }
+        else if (Input.GetKey(KeyCode.S)) {
+            direction.y = -1;
+            isMoving = true;
+        }
         rb.velocity = direction.normalized * speed;
+
+        if (isMoving) animator.SetFloat("Speed", 1);
+        else animator.SetFloat("Speed", 0);
 
         if (direction.x != 0) sr.flipX = direction.x == -1;
 
